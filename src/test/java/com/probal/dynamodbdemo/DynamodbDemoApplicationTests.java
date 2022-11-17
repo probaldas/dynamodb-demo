688712af67348784bd51f4775c169fb14670a48b
Probal Das <49045586+probaldas@users.noreply.github.com>
2022-11-16 22:35:06 -0500

    Initializing project using Spring Initializer.

---
+++ src/test/java/com/probal/dynamodbdemo/DynamodbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.probal.dynamodbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
